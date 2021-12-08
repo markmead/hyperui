f1c0bae39a3ad248036d20e4ca75f77f8cbb4ff1
markmead <itsmarkmead@gmail.com>
2021-12-08 11:31:05 +0000

    feat(analytics)

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,4 +1,5 @@
 import type { FunctionComponent } from 'react'
+import Head from 'next/head'
 
 import Header from './header'
 import Footer from './footer'
@@ -6,6 +7,24 @@ import Footer from './footer'
 const Layout: FunctionComponent = ({ children }) => {
   return (
     <>
+      <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-VE5EHLYPZP"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-VE5EHLYPZP');
+            `,
+          }}
+        />
+      </Head>
+
       <Header />
 
       <main role="main">{children}</main>
