68371fccc16067ff571d59192da50d514ec648ba
markmead <itsmarkmead@gmail.com>
2022-02-03 11:53:38 +0000

    update(components): refresh footers

--- lib/components.ts
+++ lib/components.ts
@@ -112,16 +112,15 @@ export const faqs = [
 ]
 
 export const footers = [
-  { id: 9 },
+  { id: 1 },
   { id: 2 },
-  { id: 10 },
-  { id: 7 },
-  { id: 6 },
   { id: 3 },
   { id: 4 },
-  { id: 1 },
   { id: 5 },
+  { id: 6 },
+  { id: 7 },
   { id: 8 },
+  { id: 9 },
 ]
 
 export const forms = [
