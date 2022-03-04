e53f4b39ebc08012ef07b0e1a66e6866ecb93721
markmead <itsmarkmead@gmail.com>
2022-03-04 23:21:46 +0000

    Update tabs

--- lib/components.ts
+++ lib/components.ts
@@ -212,14 +212,7 @@ export const steps = [
 
 export const tables = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]
 
-export const tabs = [
-  { id: 1 },
-  { id: 2 },
-  { id: 3 },
-  { id: 4 },
-  { id: 5 },
-  { id: 6 },
-]
+export const tabs = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]
 
 export const tags = [
   { id: 1 },
