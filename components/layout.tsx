a4938d46e091b9a2d42e60e64aeb8ceacce1cd7e
markmead <itsmarkmead@gmail.com>
2022-03-04 09:35:55 +0000

    update(site): Remove Alpine JS popup

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,7 +1,6 @@
 import { FunctionComponent } from 'react'
 
 import Announcement from './announcement'
-import Popup from './popup'
 import Header from './header'
 import Footer from './footer'
 
@@ -11,11 +10,6 @@ const Layout: FunctionComponent = ({ children }) => {
       <Announcement
         text="⭐️ Like HyperUI? Star on GitHub ⭐️"
         url="https://github.com/markmead/hyperui"
-      />
-
-      <Popup
-        text="Click for Alpine JS Snippets 🥳"
-        url="https://alpinejs.hyperui.dev"
       />
 
       <Header />
