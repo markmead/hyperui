fa1a10f6e259cd9fea4a734374e996f5af5d768c
markmead <itsmarkmead@gmail.com>
2022-03-08 12:56:51 +0000

    Use <p> tag for collection card titles

--- components/collection/card.tsx
+++ components/collection/card.tsx
@@ -18,7 +18,7 @@ const Card: FunctionComponent<Props> = ({ collection }) => {
           {emoji}
         </span>
 
-        <h2 className="mt-4 text-lg font-medium">{title}</h2>
+        <p className="mt-4 text-lg font-medium">{title}</p>
 
         <p className="mt-1 text-xs">
           {count} {count > 1 ? ' components' : ' component'}
