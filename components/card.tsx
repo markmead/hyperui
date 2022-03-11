e03e66f9fa3baf480146beefb0ab692b9488b3b2
markmead <itsmarkmead@gmail.com>
2022-03-11 22:01:33 +0000

    Add titles to components

--- components/card.tsx
+++ components/card.tsx
@@ -22,7 +22,7 @@ const Card: FunctionComponent<Props> = ({ item }) => {
         ></span>
 
         <div className="transition bg-white border-2 border-black rounded-lg group-hover:-translate-x-2 group-hover:-translate-y-2">
-          <div className="p-8">
+          <div className="p-6">
             <span className="text-xl" role="img" aria-hidden="true">
               {emoji}
             </span>
