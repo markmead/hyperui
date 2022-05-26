febc9f70f007f449f93fb4ca5f171f9a698485bd
markmead <itsmarkmead@gmail.com>
2022-05-26 09:19:12 +0100

    Fix

--- utils/component.ts
+++ utils/component.ts
@@ -3,8 +3,10 @@ export function source(html: string, spacing: string = 'relative') {
     <script>
       document.addEventListener('DOMContentLoaded', () => {
         let links = [...document.querySelectorAll('a')]
+        let forms = [...document.querySelectorAll('form')]
 
         links.forEach(link => link.addEventListener('click', (e) => e.preventDefault()))
+        forms.forEach(form => form.addEventListener('submit', (e) => e.preventDefault()))
       })
     </script>
 
