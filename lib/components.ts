9cdddd60304275f8ccda6ba0e41ba2de672014c1
markmead <itsmarkmead@gmail.com>
2022-06-23 17:26:55 +0100

    Updated - Remove unused code

--- lib/components.ts
+++ lib/components.ts
@@ -122,30 +122,3 @@ export function getCategoryBySlug(category: string, fields: string[] = []) {
 
   return items
 }
-
-// export function getMarketingComponents(fields: string[] = []) {
-//   const slugs = getComponentSlugs()
-//   const components = slugs
-//     .map((slug) => getComponentBySlug(slug, fields))
-//     .filter((component) => !component.ecommerce && !component.application)
-
-//   return components
-// }
-
-// export function getEcommerceComponents(fields: string[] = []) {
-//   const slugs = getComponentSlugs()
-//   const components = slugs
-//     .map((slug) => getComponentBySlug(slug, fields))
-//     .filter((component) => component.ecommerce)
-
-//   return components
-// }
-
-// export function getApplicationComponents(fields: string[] = []) {
-//   const slugs = getComponentSlugs()
-//   const components = slugs
-//     .map((slug) => getComponentBySlug(slug, fields))
-//     .filter((component) => component.application)
-
-//   return components
-// }
