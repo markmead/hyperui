c0430ef4e2527d4ede341401cb8f0313a01035d3
markmead <itsmarkmead@gmail.com>
2022-06-24 08:44:36 +0100

    Updated - Sort Tailwind CSS plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
   plugins: [
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
     require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/typography'),
   ],
 }
