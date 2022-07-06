0b05434549f3107cfec71c60f7e34af862d1fda5
Mark Mead <50486078+markmead@users.noreply.github.com>
2022-07-06 17:15:57 +0100

    update/site/new-popup (#114)

--- components/global/popup.tsx
+++ components/global/popup.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, useEffect, useState } from 'react'
+import { FunctionComponent } from 'react'
 
 type Props = {
   text: string
@@ -6,54 +6,30 @@ type Props = {
 }
 
 const Popup: FunctionComponent<Props> = ({ text, url }) => {
-  let [show, setShow] = useState<boolean>(true)
-
-  useEffect(() => {
-    let showPopup: string | null = localStorage.getItem('showPopup')
-
-    if (showPopup === 'FALSE') {
-      setShow(false)
-    }
-  }, [])
-
-  function hidePopup() {
-    localStorage.setItem('showPopup', 'FALSE')
-
-    setShow(false)
-  }
-
-  if (!show) return <></>
-
   return (
-    <aside className="fixed z-50 flex items-center justify-center px-5 py-3 text-white bg-black rounded-lg bottom-4 right-4">
-      <a
-        href={url}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-sm font-medium hover:opacity-75"
-      >
-        {text}
-      </a>
+    <a
+      href={url}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="fixed z-50 flex items-center max-w-xs gap-4 p-4 text-white bg-black rounded-lg shadow bottom-4 right-4"
+    >
+      <span className="text-sm font-medium">{text}</span>
 
-      <button
-        onClick={() => hidePopup()}
-        className="p-1 ml-3 rounded bg-white/20 hover:bg-white/10"
-        aria-label="Dismiss Popup"
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="w-4 h-4 shrink-0"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        strokeWidth="2"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="w-4 h-4"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fillRule="evenodd"
-            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </button>
-    </aside>
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+        />
+      </svg>
+    </a>
   )
 }
 
