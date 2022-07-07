d74a80462f9804acd01c4037b837f3f5499f09a2
markmead <itsmarkmead@gmail.com>
2022-07-07 12:26:43 +0100

    UI tweaks

--- components/content/banner.tsx
+++ components/content/banner.tsx
@@ -1,11 +1,19 @@
 import type { FunctionComponent } from 'react'
 
+import { useRouter } from 'next/router'
+
+import Image from 'next/image'
+
 type Props = {
   title: string
   subtitle?: string
 }
 
 const Banner: FunctionComponent<Props> = ({ title, subtitle, children }) => {
+  let router = useRouter()
+
+  let isHome = router.asPath === '/'
+
   return (
     <section>
       <div className="max-w-screen-xl px-4 py-16 mx-auto text-center lg:py-32">
@@ -17,6 +25,27 @@ const Banner: FunctionComponent<Props> = ({ title, subtitle, children }) => {
           <p className="max-w-lg mx-auto mt-4 text-sm text-gray-500">
             {children}
           </p>
+        )}
+
+        {isHome && (
+          <div className="mt-8 sm:justify-center sm:items-center sm:flex">
+            <p className="text-xs tracking-wide text-gray-400 uppercase">
+              Proudly supported by
+            </p>
+
+            <a
+              href="https://getcandy.io/"
+              target="_blank"
+              rel="noreferrer"
+              className="relative block w-40 h-10 mx-auto mt-4 sm:m-0"
+            >
+              <Image
+                src="/brands/getcandy.svg"
+                alt="GetCandy - Laravel eCommerce"
+                layout="fill"
+              />
+            </a>
+          </div>
         )}
       </div>
     </section>
