28b55ba0c01324c519b3cf791feaa6d569828824
Mark Mead <itsmarkmead@gmail.com>
2022-08-15 16:05:55 +0100

    Add - Header

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,17 @@ module.exports = {
     './utils/*.{js,ts}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        dropdown: 'dropdown 0.3s ease forwards',
+      },
+      keyframes: {
+        dropdown: {
+          '0%': { transform: 'translateY(-1rem)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/aspect-ratio'),
