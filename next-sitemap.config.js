629555c7ee2d15fbcd4b8117dbc9289d3b10b52e
Mark Mead <itsmarkmead@gmail.com>
2022-08-31 13:39:39 +0100

    Bump - Sitemap generator

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+
 module.exports = {
   exclude: ['/components/**/*.html'],
+  generateIndexSitemap: false,
   generateRobotsTxt: true,
   siteUrl: 'https://www.hyperui.dev',
 }
