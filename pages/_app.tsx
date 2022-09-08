b22144e3971fd60e6b7497b9f15e1444d81f06bc
Mark Mead <itsmarkmead@gmail.com>
2022-09-08 08:29:09 +0100

    Init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import type { AppProps } from 'next/app'
 
 import Head from 'next/head'
-import Script from 'next/script'
 
 import '../styles/globals.css'
 import 'prismjs/themes/prism-okaidia.css'
@@ -49,24 +48,6 @@ function MyApp({ Component, pageProps }: AppProps) {
       </Head>
 
       <Header />
-
-      <Script
-        strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-VE5EHLYPZP"
-      />
-
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-VE5EHLYPZP');
-          `,
-        }}
-      />
 
       <Layout>
         <Component {...pageProps} />
