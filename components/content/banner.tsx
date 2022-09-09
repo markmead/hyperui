a235cc8e84877e335509e4e2f559c0b5fd44a4b8
Mark Mead <itsmarkmead@gmail.com>
2022-09-09 15:54:08 +0100

    Update - URL

--- components/content/banner.tsx
+++ components/content/banner.tsx
@@ -28,9 +28,9 @@ const Banner: FunctionComponent<Props> = ({ title, subtitle, children }) => {
             </p>
 
             <a
-              href="https://getcandy.io/?ref=hyperui.dev"
+              href="https://lunarphp.io/?ref=hyperui.dev"
               target="_blank"
-              rel="noreferrer"
+              rel="noopener"
               className="relative block w-40 h-10 mx-auto mt-4 sm:m-0"
             >
               <Image
