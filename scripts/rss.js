54f46b47c3543f87a73dc9340f935942f6e0040e
Mark Mead <itsmarkmead@gmail.com>
2022-09-22 11:17:31 +0100

    Try feed pacakage

--- scripts/rss.js
+++ scripts/rss.js
@@ -2,18 +2,6 @@ const { promises: fs } = require('fs')
 const path = require('path')
 const RSS = require('rss')
 const matter = require('gray-matter')
-// const { remark } = 'remark'
-// const html = 'remark-html'
-
-async function getRemark() {
-  const { remark } = await import('remark')
-  return remark
-}
-
-async function getRemarkHtml() {
-  const html = await import('remark-html')
-  return html
-}
 
 async function generate() {
   const feed = new RSS({
@@ -24,28 +12,21 @@ async function generate() {
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'data', 'posts'))
 
-  const processor = await getRemark()
-  const egg = await getRemarkHtml()
-
   await Promise.all(
     posts.map(async (name) => {
-      if (name.startsWith('index.')) return
-
       const content = await fs.readFile(
         path.join(__dirname, '..', 'data', 'posts', name)
       )
 
       const frontmatter = matter(content)
 
-      const mdAsHtml = await processor().use(egg).process(frontmatter.content)
-
       feed.item({
         title: frontmatter.data.seo.title,
         description: frontmatter.data.seo.description,
         url: `${feed.site_url}/blog/${name.replace(/\.mdx?/, '')}`,
-        date: frontmatter.data.date,
-        author: 'HyperUI',
         categories: ['tailwindcss', 'css'],
+        author: 'HyperUI',
+        date: frontmatter.data.date,
         custom_elements: [{ content: frontmatter.content }],
       })
     })
---
+++ scripts/rss2.js
@@ -0,0 +1,55 @@
+const { promises: fs } = require('fs')
+const path = require('path')
+const Feed = require('feed').Feed
+const matter = require('gray-matter')
+
+async function generate() {
+  const feed = new Feed({
+    title: 'HyperUI | Tailwind CSS Components',
+    description: 'RSS feed for blog posts on HyperUI.',
+    id: 'https://www.hyperui.dev/',
+    link: 'https://www.hyperui.dev/',
+    language: 'en',
+    image: 'https://www.hyperui.dev/og.png',
+    favicon: 'https://www.hyperui.dev/favicon.ico',
+    feedLinks: {
+      json: 'https://www.hyperui.dev/json',
+      atom: 'https://www.hyperui.dev/atom',
+    },
+    author: {
+      name: 'Mark Mead',
+      link: 'https://www.markmead.dev/',
+    },
+  })
+
+  const posts = await fs.readdir(path.join(__dirname, '..', 'data', 'posts'))
+
+  await Promise.all(
+    posts.map(async (name) => {
+      const content = await fs.readFile(
+        path.join(__dirname, '..', 'data', 'posts', name)
+      )
+
+      const frontmatter = matter(content)
+
+      feed.addItem({
+        title: frontmatter.data.seo.title,
+        id: frontmatter.slug,
+        link: frontmatter.slug,
+        description: frontmatter.data.seo.description,
+        content: frontmatter.content,
+        author: [
+          {
+            name: 'Mark Mead',
+            link: 'https://www.markmead.dev/',
+          },
+        ],
+        date: new Date(frontmatter.data.date),
+      })
+    })
+  )
+
+  await fs.writeFile('./public/feed2.xml', feed.rss2())
+}
+
+generate()
