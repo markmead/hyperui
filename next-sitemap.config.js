9d9629702d5c87d1685bc4fffa64ec329227af19
Mark Mead <itsmarkmead@gmail.com>
2022-09-26 17:45:13 +0100

    Disallow search json from being indexed

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,5 +4,14 @@ module.exports = {
   exclude: ['/components/**/*.html'],
   generateIndexSitemap: false,
   generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/search.json'],
+      },
+    ],
+  },
   siteUrl: 'https://www.hyperui.dev',
 }
