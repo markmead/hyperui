4fbc73ac8ee94f617e3ef13e44892d2ee072336f
Mark Mead <itsmarkmead@gmail.com>
2022-09-26 17:41:21 +0100

    Fix build issue

--- components/global/search.tsx
+++ components/global/search.tsx
@@ -29,6 +29,7 @@ const Search: FunctionComponent = () => {
     })
 
     setSearchResults(filteredResults)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [searchQuery])
 
   useEffect(() => {
