04641f53f11d83375e13196b24f17523cbb4b838
Mark Mead <itsmarkmead@gmail.com>
2022-10-03 13:58:54 +0100

    Initial work with the iframe

--- src/components/PreviewIframe.tsx
+++ src/components/PreviewIframe.tsx
@@ -1,4 +1,4 @@
-import { RefObject } from 'react'
+import { RefObject, useState } from 'react'
 
 type Props = {
   showPreview: boolean
@@ -15,15 +15,28 @@ function PreviewIframe({
   previewWidth,
   refIframe,
 }: Props) {
+  const [previewHeight, setPreviewHeight] = useState<string>('400px')
+
+  function handleIframeLoaded(targetEl: EventTarget) {
+    const iframeEl = targetEl as HTMLIFrameElement
+    const iframeElHeight =
+      iframeEl?.contentWindow?.document.body.scrollHeight ?? 0
+
+    iframeElHeight > 800
+      ? setPreviewHeight('800px')
+      : setPreviewHeight(`${iframeElHeight}px`)
+  }
+
   return (
     <div className={showPreview ? 'block' : 'hidden'}>
       <iframe
-        className="h-[400px] w-full rounded-lg bg-white ring-2 ring-black lg:h-[600px] lg:transition-all"
+        className="w-full rounded-lg bg-white ring-2 ring-black lg:transition-all"
         loading="lazy"
         srcDoc={componentHtml}
-        style={{ maxWidth: previewWidth }}
+        style={{ maxWidth: previewWidth, minHeight: previewHeight }}
         title={`${componentTitle} Component`}
         ref={refIframe}
+        onLoad={(e) => handleIframeLoaded(e.target)}
       ></iframe>
     </div>
   )
