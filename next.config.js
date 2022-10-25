1a22978915d3769978ab79abaa3590bdd78d4d0c
Mark Mead <itsmarkmead@gmail.com>
2022-10-25 11:10:26 +0100

    Try rewrite for preview

--- next.config.js
+++ next.config.js
@@ -5,4 +5,13 @@ module.exports = {
   experimental: {
     scrollRestoration: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination:
+          'https://hyperui-git-update-use-api-markmead.vercel.app/api/:path*',
+      },
+    ]
+  },
 }
