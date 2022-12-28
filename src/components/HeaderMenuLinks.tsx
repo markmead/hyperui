951bedb1d62135e38a3431da6c323ffb40f52a54
Mark Mead <itsmarkmead@gmail.com>
2022-12-28 17:58:35 +0000

    Remove extra div

--- src/components/HeaderMenuLinks.tsx
+++ src/components/HeaderMenuLinks.tsx
@@ -10,31 +10,29 @@ type Props = {
 
 function HeaderMenuLinks({ menuLinks, navClass, ulClass }: Props) {
   return (
-    <div>
-      <nav aria-label="Page Nav" className={navClass && navClass}>
-        <ul className={ulClass && ulClass}>
-          {menuLinks.map((menuLink, index, { length }) => {
-            const isLast = index === length - 1
+    <nav aria-label="Page Nav" className={navClass && navClass}>
+      <ul className={ulClass && ulClass}>
+        {menuLinks.map((menuLink, index, { length }) => {
+          const isLast = index === length - 1
 
-            return (
-              <li key={menuLink.href} className={isLast ? 'lg:ml-auto' : ''}>
-                <Link href={menuLink.href}>
-                  <a
-                    className="block text-xs font-medium hover:opacity-75"
-                    {...(menuLink.external && {
-                      target: '_blank',
-                      rel: 'noreferrer',
-                    })}
-                  >
-                    {menuLink.title}
-                  </a>
-                </Link>
-              </li>
-            )
-          })}
-        </ul>
-      </nav>
-    </div>
+          return (
+            <li key={menuLink.href} className={isLast ? 'lg:ml-auto' : ''}>
+              <Link href={menuLink.href}>
+                <a
+                  className="block text-xs font-medium hover:opacity-75"
+                  {...(menuLink.external && {
+                    target: '_blank',
+                    rel: 'noreferrer',
+                  })}
+                >
+                  {menuLink.title}
+                </a>
+              </Link>
+            </li>
+          )
+        })}
+      </ul>
+    </nav>
   )
 }
 
