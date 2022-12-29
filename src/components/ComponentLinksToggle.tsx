78582e36c2f9c5530d8b20d518acb973565faea6
Mark Mead <itsmarkmead@gmail.com>
2022-12-29 10:28:55 +0000

    Tweak the toggle component

--- src/components/ComponentLinksToggle.tsx
+++ src/components/ComponentLinksToggle.tsx
@@ -20,34 +20,37 @@ function ComponentLinksToggle() {
 
   return (
     <div className="fixed inset-x-0 bottom-0 z-50 p-2">
-      <div className="max-w-sm p-4 ml-auto bg-white border border-gray-100 rounded-lg shadow-sm">
-        <form>
-          <div className="flex items-center gap-4">
-            <p className="text-xs font-medium">
-              Show component links on this page?
+      <div className="max-w-sm p-4 ml-auto space-y-4 overflow-hidden bg-white border border-gray-100 rounded-lg shadow-lg">
+        <div className="flex items-start gap-4">
+          <div>
+            <p className="text-sm font-medium">Show Component Links</p>
+
+            <p className="mt-1 text-xs text-gray-700">
+              Enabling this will show all the component links on this page for
+              quick navigation between pages.
             </p>
-
-            <label
-              htmlFor="ShowLinks"
-              className="relative h-8 cursor-pointer w-14"
-            >
-              <input
-                type="checkbox"
-                id="ShowLinks"
-                className="sr-only peer"
-                checked={showLinks}
-                onChange={() => setShowLinks(!showLinks)}
-              />
-
-              <span className="absolute inset-0 transition bg-gray-300 rounded-full peer-checked:bg-green-500"></span>
-
-              <span className="absolute inset-0 w-6 h-6 m-1 transition bg-white rounded-full peer-checked:translate-x-6"></span>
-            </label>
           </div>
-        </form>
 
-        <p className="mt-4 text-xs font-medium text-gray-700">
-          This is an experimental feature, have your say on{' '}
+          <label
+            htmlFor="ShowLinks"
+            className="relative w-12 cursor-pointer h-7 shrink-0"
+          >
+            <input
+              type="checkbox"
+              id="ShowLinks"
+              className="sr-only peer"
+              checked={showLinks}
+              onChange={() => setShowLinks(!showLinks)}
+            />
+
+            <span className="absolute inset-0 transition bg-gray-300 rounded-full peer-checked:bg-green-500"></span>
+
+            <span className="absolute inset-0 w-5 h-5 m-1 transition bg-white rounded-full peer-checked:translate-x-5"></span>
+          </label>
+        </div>
+
+        <p className="text-xs font-medium text-gray-700">
+          Have thoughts on this feature? Share them on{' '}
           <a
             href="https://github.com/markmead/hyperui/pull/229"
             target="_blank"
