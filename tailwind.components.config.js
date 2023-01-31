93415b584eb4cfa5dc10e55fbb508f52cb015179
Mark Mead <itsmarkmead@gmail.com>
2023-01-31 14:42:24 +0000

    Finish removing old variants

--- tailwind.components.config.js
+++ tailwind.components.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ['./public/**/*.html'],
+  content: ['./public/**/*.html', './src/**/*.mdx'],
   theme: {
     extend: {},
   },
