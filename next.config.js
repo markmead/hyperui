a9a0bc1da4192ee2800f3438f0c06b5f661506c7
Mark Mead <itsmarkmead@gmail.com>
2023-02-03 18:49:42 +0000

    Initial commit

--- next.config.js
+++ next.config.js
@@ -5,4 +5,29 @@ module.exports = {
   experimental: {
     scrollRestoration: true,
   },
+
+  async redirects() {
+    return [
+      {
+        source: '/components',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/marketing',
+        destination: '/components/marketing',
+        permanent: true,
+      },
+      {
+        source: '/ecommerce',
+        destination: '/components/ecommerce',
+        permanent: true,
+      },
+      {
+        source: '/application-ui',
+        destination: '/components/application-ui',
+        permanent: true,
+      },
+    ]
+  },
 }
