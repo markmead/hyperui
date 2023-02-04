db5fda853ba6ac083a64616048e56304d41249a3
Mark Mead <50486078+markmead@users.noreply.github.com>
2023-02-04 10:20:50 +0000

    Feature - Add redirects for removed blogs (#249)

--- next.config.js
+++ next.config.js
@@ -28,6 +28,16 @@ module.exports = {
         destination: '/components/application-ui',
         permanent: true,
       },
+      {
+        source: '/blog/whats-new-in-hyperui',
+        destination: '/blog',
+        permanent: true,
+      },
+      {
+        source: '/blog/hyperui-rewrite-whats-changed',
+        destination: '/blog',
+        permanent: true,
+      },
     ]
   },
 }
