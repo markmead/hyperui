b3035ef731c479e07e750a00020d96aa0cc6574e
Mark Mead <itsmarkmead@gmail.com>
2023-02-04 10:57:41 +0000

    Fix missing forms plugin

--- tailwind.site.config.js
+++ tailwind.site.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.tsx'],
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 }
