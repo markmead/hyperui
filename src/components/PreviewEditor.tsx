025a57db989cd564c3696229ad7ab3fd9aacb8bc
Mark Mead <itsmarkmead@gmail.com>
2023-02-06 19:44:26 +0000

    Prevent empty code being saved

--- src/components/PreviewEditor.tsx
+++ src/components/PreviewEditor.tsx
@@ -28,18 +28,42 @@ export default function PreviewEditor({
   componentCode,
   handleEditCode,
 }: Props) {
+  const [staticCode, setStaticCode] = useState<string>('')
   const [editCode, setEditCode] = useState<string>('')
+  const [errorMessage, setErrorMessage] = useState<string>('')
 
   useEffect(() => {
     setEditCode(componentCode)
+    setStaticCode(componentCode)
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
+  useEffect(() => {
+    if (!errorMessage) {
+      return
+    }
+
+    setTimeout(() => setErrorMessage(''), 3000)
+  }, [errorMessage])
+
+  function saveCode() {
+    const trimmedCode = editCode.trim()
+
+    if (!trimmedCode) {
+      setEditCode(staticCode)
+      setErrorMessage('You cannot save empty code 🙅‍♀️')
+
+      return
+    }
+
+    handleEditCode(sanitizeHtml(editCode, sanitizeOptions))
+  }
+
   return (
     <div className="relative h-[400px] overflow-auto rounded-lg bg-gray-900 ring-2 ring-gray-900 lg:h-[600px]">
       <button
-        onClick={() => handleEditCode(sanitizeHtml(editCode, sanitizeOptions))}
+        onClick={() => saveCode()}
         className={`${styles.pill} absolute bottom-4 right-4 z-50 bg-black text-white`}
       >
         <span aria-hidden="true" role="img" className="text-sm">
@@ -48,6 +72,12 @@ export default function PreviewEditor({
 
         <span className="text-xs font-medium">Save</span>
       </button>
+
+      {errorMessage && (
+        <div className="absolute inset-0 z-50 grid place-content-center bg-gray-900">
+          <p className="font-medium text-red-600">{errorMessage}</p>
+        </div>
+      )}
 
       <CodeEditor
         // @ts-ignore
