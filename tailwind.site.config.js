2ada121fc9aa761dcb99c05541b2aa6a765c75fb
Mark Mead <itsmarkmead@gmail.com>
2023-02-07 16:19:51 +0000

    Initial commit

--- tailwind.site.config.js
+++ tailwind.site.config.js
@@ -1,8 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,tsx,mdx}'],
-  theme: {
-    extend: {},
-  },
+  content: ['./src/**/*.tsx'],
   plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 }
