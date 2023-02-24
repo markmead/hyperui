ceda4f3b230a5fea78eb855d3dfa45bed034e284
Mark Mead <itsmarkmead@gmail.com>
2023-02-24 08:48:20 +0000

    All blogs

--- tailwind/blogs.config.js
+++ tailwind/blogs.config.js
@@ -1,4 +1,29 @@
+const plugin = require('tailwindcss/plugin')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./public/blogs/**/*.html'],
+  theme: {
+    extend: {
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
+  ],
+  presets: [require('./components.preset.js')],
 }
