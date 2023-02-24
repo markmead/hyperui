20411d044efd28bc4d9da761bcc0023c92d60dbb
Mark Mead <itsmarkmead@gmail.com>
2023-02-24 09:38:48 +0000

    Fix text-shadow classes

--- tailwind/extras.preset.js
+++ tailwind/extras.preset.js
@@ -1,16 +1,7 @@
-const plugin = require('tailwindcss/plugin')
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   theme: {
     extend: {
-      extend: {
-        textShadow: {
-          sm: '0 1px 2px var(--tw-shadow-color)',
-          DEFAULT: '0 2px 4px var(--tw-shadow-color)',
-          lg: '0 8px 16px var(--tw-shadow-color)',
-        },
-      },
       animation: {
         background: 'background ease infinite',
       },
@@ -21,17 +12,5 @@ module.exports = {
         },
       },
     },
-    plugins: [
-      plugin(function ({ matchUtilities, theme }) {
-        matchUtilities(
-          {
-            'text-shadow': (value) => ({
-              textShadow: value,
-            }),
-          },
-          { values: theme('textShadow') }
-        )
-      }),
-    ],
   },
 }
