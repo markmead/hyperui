62b8f166bd34d64476d843d12899e7827fed29bc
Mark Mead <itsmarkmead@gmail.com>
2023-03-12 19:30:39 +0000

    Initial commit

---
+++ src/components/SettingBionicMode.tsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from 'react'
+
+import { useRouter } from 'next/router'
+
+import { useAppSelector, useAppDispatch } from '@/services/hooks/useStore'
+import { toggleBionic, settingsState } from '@/services/store/slices/settings'
+
+import SettingTitle from '@/components/SettingTitle'
+import SettingToggle from '@/components/SettingToggle'
+
+function SettingBionicMode() {
+  const dispatch = useAppDispatch()
+  const { bionic } = useAppSelector(settingsState)
+
+  const { asPath } = useRouter()
+
+  const [initialBionic, setInitialBionic] = useState(bionic)
+  const [shouldRefresh, setShouldRefresh] = useState(false)
+
+  useEffect(() => {
+    setInitialBionic(bionic)
+    setShouldRefresh(false)
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [asPath])
+
+  useEffect(() => {
+    const valueHasChanged = initialBionic !== bionic
+
+    setShouldRefresh(valueHasChanged)
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [bionic])
+
+  return (
+    <div>
+      <div className="flex items-start gap-4">
+        <div className="flex-1">
+          <SettingTitle
+            settingTitle="Bionic Mode"
+            settingDescription="Components will be rendered in bionic mode by default if they have an bionic mode variant."
+            shouldRefresh={shouldRefresh}
+          />
+        </div>
+
+        <div className="shrink-0">
+          <SettingToggle
+            toggleValue={bionic}
+            toggleHandler={() => dispatch(toggleBionic())}
+            toggleId="BionicMode"
+            toggleLabel="Default Bionic Mode"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SettingBionicMode
