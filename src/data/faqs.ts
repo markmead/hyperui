71404feb530880291ee19356411c7efd05913bbc
Mark Mead <50486078+markmead@users.noreply.github.com>
2023-04-20 09:21:49 +0100

    Feature - Content FAQs (#293)

---
+++ src/data/faqs.ts
@@ -0,0 +1,44 @@
+import { FaqItem } from '@/interface/global'
+
+export const faqItems: Array<FaqItem> = [
+  {
+    question: 'What is HyperUI?',
+    answer:
+      'HyperUI is a collection of free Tailwind CSS components that can be used in your next project. With a range of components, you can build your next marketing website, admin dashboard, eCommerce store and much more.',
+  },
+  {
+    question: 'Is HyperUI free?',
+    answer:
+      'Yes, HyperUI is completely free to use. You can use it in your personal or commercial projects.',
+  },
+  {
+    question: 'How do I use HyperUI?',
+    answer:
+      'Browse the website for the components you need, copy the code and paste it into your project. Some components will have support for dark mode and Alpine JS variants.',
+  },
+  {
+    question: 'Do I need to credit HyperUI?',
+    answer:
+      'No, you do not need to credit HyperUI. However, it would be appreciated if you could star the project on GitHub and/or share it on Twitter.',
+  },
+  {
+    question: 'How can I support HyperUI?',
+    answer:
+      'You can support HyperUI by starring the project on GitHub and/or sharing it on Twitter. It helps me to know that people are using the project and it increases the visibility of the project.',
+  },
+  {
+    question: 'How do I get Support?',
+    answer:
+      'There is no official support for HyperUI. However, I am happy to help out. You can reach me on GitHub by creating an issue or discussion.',
+  },
+  {
+    question: 'How do I contribute?',
+    answer:
+      'You can contribute by adding new components, fixing bugs or improving the documentation. Please create an issue before starting any work.',
+  },
+  {
+    question: 'How do I get updates?',
+    answer:
+      'I no longer use Twitter, therefore the best way to get updates is by following the project on GitHub.',
+  },
+]
---
+++ src/pages/about/faqs.tsx
@@ -0,0 +1,81 @@
+import Head from 'next/head'
+
+import Banner from '@/components/HeroBanner'
+
+import { faqItems } from '@/data/faqs'
+import { FaqItem } from '@/interface/global'
+
+function Faqs() {
+  const schemaData = {
+    '@context': 'http://schema.org',
+    '@type': 'FAQPage',
+    mainEntity: [
+      faqItems.map((faqItem: FaqItem) => {
+        return {
+          '@type': 'Question',
+          name: faqItem.question,
+          acceptedAnswer: {
+            '@type': 'Answer',
+            text: faqItem.answer,
+          },
+        }
+      }),
+    ],
+  }
+
+  return (
+    <>
+      <Head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaData) }}
+        />
+        <title>FAQs | HyperUI</title>
+        <meta
+          name="description"
+          content="Get your questions about HyperUI answered with these FAQs."
+          key="description"
+        />
+        <meta property="og:title" content="FAQs | HyperUI" key="og:title" />
+        <meta
+          property="og:description"
+          content="Get your questions about HyperUI answered with these FAQs."
+          key="og:description"
+        />
+        <meta
+          name="twitter:title"
+          content="FAQs | HyperUI"
+          key="twitter:title"
+        />
+        <meta
+          name="twitter:description"
+          content="Get your questions about HyperUI answered with these FAQs."
+          key="twitter:description"
+        />
+      </Head>
+
+      <Banner title="FAQs" subtitle="Quick Info for HyperUI">
+        Got questions? I have answers. Hopefully these answer any questions you
+        have about HyperUI. If not, then please reach out on GitHub.
+      </Banner>
+
+      <div className="mx-auto max-w-screen-xl px-4 py-12">
+        <ul className="-mt-4 grid gap-px bg-gray-100 sm:-mt-6 sm:grid-cols-2 lg:-mt-8">
+          {faqItems.map((faqItem: FaqItem) => {
+            return (
+              <li className="bg-white p-4 sm:p-6 lg:p-8" key={faqItem.question}>
+                <h2 className="text-lg font-medium text-gray-900">
+                  {faqItem.question}
+                </h2>
+
+                <p className="mt-4 text-gray-600">{faqItem.answer}</p>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default Faqs
