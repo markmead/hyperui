6ca5d7cba119bedbbe6bd8b6951fa654d94b1497
Mark Mead <itsmarkmead@gmail.com>
2023-04-20 09:15:08 +0100

    Include SEO

--- src/pages/about/faqs.tsx
+++ src/pages/about/faqs.tsx
@@ -1,11 +1,59 @@
+import Head from 'next/head'
+
 import Banner from '@/components/HeroBanner'
 
 import { faqItems } from '@/data/faqs'
 import { FaqItem } from '@/interface/global'
 
 function Faqs() {
+  const schemaData = {
+    '@context': 'http://schema.org',
+    '@type': 'FAQPage',
+    mainEntity: [
+      faqItems.map((faqItem: FaqItem) => {
+        return {
+          '@type': 'Question',
+          name: faqItem.question,
+          acceptedAnswer: {
+            '@type': 'Answer',
+            text: faqItem.answer,
+          },
+        }
+      }),
+    ],
+  }
+
   return (
     <>
+      <Head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaData) }}
+        />
+        <title>FAQs | HyperUI</title>
+        <meta
+          name="description"
+          content="Get your questions about HyperUI answered with these FAQs."
+          key="description"
+        />
+        <meta property="og:title" content={`$FAQs | HyperUI`} key="og:title" />
+        <meta
+          property="og:description"
+          content="Get your questions about HyperUI answered with these FAQs."
+          key="og:description"
+        />
+        <meta
+          name="twitter:title"
+          content={`$FAQs | HyperUI`}
+          key="twitter:title"
+        />
+        <meta
+          name="twitter:description"
+          content="Get your questions about HyperUI answered with these FAQs."
+          key="twitter:description"
+        />
+      </Head>
+
       <Banner title="FAQs" subtitle="Quick Info for HyperUI">
         Got questions? I've got answers. Hopefully these answer any questions
         you have about HyperUI. If not, then please reach out on GitHub.
