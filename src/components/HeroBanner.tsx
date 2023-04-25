847fa395411569d6cae8ba268a87160220e3ef02
Mark Mead <itsmarkmead@gmail.com>
2023-04-25 20:01:50 +0100

    Update HeroBanner.tsx

--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -9,17 +9,19 @@ type Props = {
 function HeroBanner({ children, subtitle, title }: Props) {
   return (
     <section className="bg-white dark:bg-gray-900">
-      <div className="mx-auto max-w-screen-xl px-4 py-16 text-center lg:py-32">
-        <h1 className="text-4xl font-bold text-gray-900 dark:text-white sm:text-6xl">
-          {title}
-        </h1>
+      <div className="mx-auto max-w-screen-xl px-4 py-8 lg:py-12">
+        <div className="flex flex-col">
+          <h1 className="order-last mt-4 text-gray-700 dark:text-gray-200">
+            {subtitle}
+          </h1>
 
-        {subtitle && (
-          <h2 className="mt-4 text-gray-700 dark:text-gray-200">{subtitle}</h2>
-        )}
+          <h2 className="text-4xl font-bold text-gray-900 dark:text-white sm:text-6xl">
+            {title}
+          </h2>
+        </div>
 
         {children && (
-          <p className="mx-auto mt-4 max-w-lg text-sm/relaxed text-gray-500 dark:text-gray-400">
+          <p className="mt-6 max-w-lg text-sm/relaxed text-gray-600 dark:text-gray-400">
             {children}
           </p>
         )}
