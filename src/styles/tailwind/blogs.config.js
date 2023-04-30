971768b7ac6372666ef7b5bf0abb32a39c393590
Mark Mead <itsmarkmead@gmail.com>
2023-04-30 19:03:20 +0100

    Fix CSS

--- src/styles/tailwind/blogs.config.js
+++ src/styles/tailwind/blogs.config.js
@@ -3,7 +3,7 @@ const plugin = require('tailwindcss/plugin')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ['./public/blogs/*.html', './data/posts/*.mdx'],
+  content: ['./public/blogs/*.html', './src/data/posts/*.mdx'],
   theme: {
     textShadow: {
       sm: '0 1px 2px var(--tw-shadow-color)',
--- src/styles/tailwind/site.config.js
+++ src/styles/tailwind/site.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.jsx', './components/**/*.jsx'],
+  content: ['./app/**/*.jsx', './src/components/**/*.jsx'],
   plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 }
