4f50cc3c36fad62f80d28693d411e997259c7a16
Mark Mead <itsmarkmead@gmail.com>
2023-05-01 12:09:54 +0100

    Shrink spacing

--- src/components/CollectionList.jsx
+++ src/components/CollectionList.jsx
@@ -5,7 +5,7 @@ import ComponentPreview from '@component/ComponentPreview'
 export default function CollectionList({ componentsData, componentContainer }) {
   return (
     <div className="not-prose mx-auto xl:max-w-[1348px]">
-      <ul className="space-y-16">
+      <ul className="space-y-8 lg:space-y-12">
         {componentsData.map((componentData) => (
           <li key={componentData.id}>
             <ComponentPreview
