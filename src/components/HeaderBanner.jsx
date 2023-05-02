c7f51e41b6ae5f03e68dd9e62ba9d21dd1f35db8
Mark Mead <itsmarkmead@gmail.com>
2023-05-02 13:59:25 +0100

    Use real banner

--- src/components/HeaderBanner.jsx
+++ src/components/HeaderBanner.jsx
@@ -5,21 +5,6 @@ export default function HeaderBanner() {
     <section className="-mt-px border-y border-gray-200 bg-gray-100">
       <Container classNames="py-1.5">
         <a
-          href="https://github.com/markmead/hyperui/issues/new/choose"
-          rel="noreferrer"
-          target="_blank"
-          className="flex items-center justify-center gap-1.5 transition hover:opacity-75"
-        >
-          <span className="text-sm/tight font-medium">
-            HyperUI is now on Next JS 13! Please report any issues you find
-          </span>
-
-          <span aria-hidden="true" role="img">
-            🚀
-          </span>
-        </a>
-
-        {/* <a
           href="https://github.com/markmead/hyperui"
           rel="noreferrer"
           target="_blank"
@@ -32,7 +17,7 @@ export default function HeaderBanner() {
           <span aria-hidden="true" role="img">
             🎉
           </span>
-        </a> */}
+        </a>
       </Container>
     </section>
   )
