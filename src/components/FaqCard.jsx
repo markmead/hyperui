01350702cd58a9dd62ec930c1574794fe27c9dbd
Mark Mead <itsmarkmead@gmail.com>
2023-05-06 16:17:15 +0100

    Update FaqCard.jsx

--- src/components/FaqCard.jsx
+++ src/components/FaqCard.jsx
@@ -1,9 +1,9 @@
 export default function FaqCard({ faqQuestion, faqAnswer }) {
   return (
-    <li className="bg-white py-4 lg:py-8 lg:odd:pe-8 lg:even:ps-8">
+    <div className="py-4 lg:py-8">
       <h2 className="text-lg font-medium text-gray-900">{faqQuestion}</h2>
 
       <p className="mt-4 text-gray-600">{faqAnswer}</p>
-    </li>
+    </div>
   )
 }
