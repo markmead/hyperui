9306616633f6dc826dd63fb244dbca67f67e3585
Mark Mead <itsmarkmead@gmail.com>
2023-05-24 21:51:20 +0100

    Initial commit

--- src/components/PreviewIframe.jsx
+++ src/components/PreviewIframe.jsx
@@ -1,3 +1,7 @@
+import { useState } from 'react'
+
+import { useEvent } from 'react-use'
+
 export default function PreviewIframe({
   showPreview,
   componentHtml,
@@ -6,17 +10,39 @@ export default function PreviewIframe({
   refIframe,
   previewDark,
 }) {
+  const [iframeHeight, setIframeHeight] = useState(0)
+
+  function handleLoad() {
+    if (!refIframe.current) return
+
+    if (iframeHeight) return
+
+    setIframeHeight(refIframe.current.contentWindow.document.body.scrollHeight)
+  }
+
+  function handleResize() {
+    if (!refIframe.current) return
+
+    setIframeHeight(refIframe.current.contentWindow.document.body.scrollHeight)
+  }
+
+  useEvent('resize', handleResize, window, { capture: true })
+
   return (
     <div className={showPreview ? 'block' : 'hidden'}>
       <iframe
-        className={`h-[400px] w-full rounded-lg ring-2 ring-gray-900 lg:h-[600px] lg:transition-all ${
+        className={`w-full rounded-lg ring-2 ring-gray-900 lg:transition-all ${
           previewDark ? 'bg-gray-950' : 'bg-white'
         }`}
         loading="lazy"
         srcDoc={componentHtml}
-        style={{ maxWidth: previewWidth }}
+        style={{
+          maxWidth: previewWidth,
+          height: iframeHeight ? iframeHeight : '400px',
+        }}
         title={`${componentTitle} Component`}
         ref={refIframe}
+        onLoad={handleLoad}
       ></iframe>
     </div>
   )
