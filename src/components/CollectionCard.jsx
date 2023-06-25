9b95004a32610d650e00e9855aae615b9b58b3a9
Mark Mead <50486078+markmead@users.noreply.github.com>
2023-06-25 14:38:32 +0100

    Hotfix - General clean up (#329)

--- src/components/CollectionCard.jsx
+++ src/components/CollectionCard.jsx
@@ -5,14 +5,24 @@ export default function CollectionCard({ componentData }) {
     componentData.count > 1 ? 'Components' : 'Component'
   const componentCount = `${componentData.count} ${componentCountPluralize}`
 
+  const hasTag = !!componentData.tag
+
   return (
     <Link href={`/components/${componentData.category}/${componentData.slug}`}>
       <div className="group relative block h-full bg-white before:absolute before:inset-0 before:rounded-lg before:border-2 before:border-dashed before:border-gray-900">
         <div className="rounded-lg border-2 border-gray-900 bg-white transition group-hover:-translate-y-2 ltr:group-hover:-translate-x-2 rtl:group-hover:translate-x-2">
           <div className="p-4 sm:p-6">
-            <span aria-hidden="true" role="img" className="text-lg sm:text-xl">
-              {componentData.emoji}
-            </span>
+            <div className="flex items-start justify-between">
+              <span
+                aria-hidden="true"
+                role="img"
+                className="text-lg sm:text-xl"
+              >
+                {componentData.emoji}
+              </span>
+
+              {hasTag && <CardTag tagType={componentData.tag} />}
+            </div>
 
             <h2 className="mt-4 font-medium text-gray-900 sm:text-lg">
               {componentData.title}
@@ -23,5 +33,24 @@ export default function CollectionCard({ componentData }) {
         </div>
       </div>
     </Link>
+  )
+}
+
+function CardTag({ tagType }) {
+  const isNew = tagType === 'new'
+  const isUpdated = tagType === 'updated'
+
+  if (!isNew && !isUpdated) {
+    return <></>
+  }
+
+  return (
+    <span
+      className={`-me-1.5 -mt-1.5 whitespace-nowrap rounded-full px-2.5 py-0.5 text-xs font-medium capitalize sm:-me-3 sm:-mt-3 ${
+        isNew && 'bg-green-100 text-green-700'
+      } ${isUpdated && 'bg-blue-100 text-blue-700'}`}
+    >
+      {tagType}
+    </span>
   )
 }
--- src/components/PreviewIframe.jsx
+++ src/components/PreviewIframe.jsx
@@ -13,6 +13,7 @@ export default function PreviewIframe({
       {...(!showPreview && {
         hidden: true,
       })}
+      className="rounded-lg bg-white bg-[linear-gradient(45deg,_rgb(249_250_251)_25%,_transparent_25%),_linear-gradient(-45deg,_rgb(249_250_251)_25%,_transparent_25%),_linear-gradient(45deg,_transparent_75%,_rgb(249_250_251)_75%),_linear-gradient(-45deg,_transparent_75%,_rgb(249_250_251)_75%)] bg-[length:_20px_20px] [background-position:_0_0,_0_10px,_10px_-10px,_-10px_0px] "
     >
       <iframe
         className={`h-[400px] w-full rounded-lg ring-2 ring-gray-900 lg:h-[600px] lg:transition-all ${iframeTheme}`}
