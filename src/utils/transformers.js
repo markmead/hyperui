7e8378b67236d09fc14cf2696ffea243440c6309
Mark Mead <50486078+markmead@users.noreply.github.com>
2023-06-25 13:18:07 +0100

    Hotfix - Bump packages (#328)

--- src/utils/transformers.js
+++ src/utils/transformers.js
@@ -58,7 +58,7 @@ export function blogPreviewHtml(
   return `
     <html class="${htmlClass}">
       <head>
-        <link rel="stylesheet" href="/components.css">
+        <link rel="stylesheet" href="/blogs.css">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
