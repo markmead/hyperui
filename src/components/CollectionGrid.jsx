804aae45f68c9e2f5d34558be56ee77ef8595306
Mark Mead <itsmarkmead@gmail.com>
2023-07-20 10:03:45 +0100

    Initial commit

--- src/components/CollectionGrid.jsx
+++ src/components/CollectionGrid.jsx
@@ -4,7 +4,7 @@ export default function CollectionGrid({ componentItems }) {
   return (
     <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {componentItems.map((componentData) => (
-        <li key={componentData.slug}>
+        <li key={`${componentData.category}-${componentData.slug}`}>
           <CollectionCard componentData={componentData} />
         </li>
       ))}
