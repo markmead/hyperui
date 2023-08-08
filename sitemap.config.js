f73cda4bc38cdcded22131f384c24380ff63c17c
Mark Mead <50486078+markmead@users.noreply.github.com>
2023-08-08 17:15:05 +0100

    Hotfix - Sitemap (#353)

--- sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  exclude: ['/components/**/*.html', '/blogs/*.html'],
-  generateIndexSitemap: false,
-  generateRobotsTxt: true,
-  siteUrl: 'https://www.hyperui.dev',
-}
