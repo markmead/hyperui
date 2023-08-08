0bc9f3b6b9d4f9c62ebe2c08048c912c09e0e862
Mark Mead <itsmarkmead@gmail.com>
2023-08-08 15:59:48 +0100

    Remove files from sitemap

--- sitemap.config.js
+++ sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  exclude: ['/components/**/*.html'],
+  exclude: ['/components/**/*.html', '/blogs/*.html'],
   generateIndexSitemap: false,
   generateRobotsTxt: true,
   siteUrl: 'https://www.hyperui.dev',
