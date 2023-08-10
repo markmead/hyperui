0cc12921feec0eaa0fb6ed3ef60451ef08fc1309
Mark Mead <itsmarkmead@gmail.com>
2023-08-10 10:11:30 +0100

    Add page

---
+++ app/about/acknowledgements/page.jsx
@@ -0,0 +1,71 @@
+import Container from '@component/Container'
+import HeroBanner from '@component/HeroBanner'
+
+export const metadata = {
+  title: 'Acknowledgements | HyperUI',
+  description: 'Saying thanks to the tools used to make HyperUI.',
+  openGraph: {
+    title: 'Acknowledgements | HyperUI',
+    description: 'Saying thanks to the tools used to make HyperUI.',
+    url: 'https://www.hyperui.dev/',
+    siteName: 'HyperUI',
+    type: 'website',
+    image: 'https://www.hyperui.dev/og.jpg',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Acknowledgements | HyperUI',
+    description: 'Saying thanks to the tools used to make HyperUI.',
+  },
+}
+
+export default async function Page() {
+  const pageLinks = [
+    {
+      name: 'PrismJS',
+      url: 'https://github.com/PrismJS/prism',
+    },
+    {
+      name: 'React Use',
+      url: 'https://github.com/streamich/react-use',
+    },
+    {
+      name: 'Next MDX Remote',
+      url: 'https://github.com/hashicorp/next-mdx-remote',
+    },
+    {
+      name: 'Prettier (TailwindCSS)',
+      url: 'https://github.com/tailwindlabs/prettier-plugin-tailwindcss',
+    },
+    {
+      name: 'React Intersection Observer',
+      url: 'https://github.com/thebuilder/react-intersection-observer',
+    },
+  ]
+
+  return (
+    <>
+      <Container classNames="py-8 lg:py-12">
+        <article class="prose">
+          <h1>Acknowledgements</h1>
+
+          <p>
+            These are not all of the tools used to make HyperUI, but these are
+            the ones that I think deserve a mention as they could easily fly
+            under the radar, but in fact are very important to the project.
+          </p>
+
+          <ul>
+            {pageLinks.map((pageLink) => (
+              <li>
+                <a href={pageLink.url} target="_blank" rel="noreferrer">
+                  {pageLink.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </article>
+      </Container>
+    </>
+  )
+}
