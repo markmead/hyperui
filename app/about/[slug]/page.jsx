6356b959ed366ff581ba39c870df9e7e14b24e2e
Mark Mead <itsmarkmead@gmail.com>
2023-08-13 07:04:33 +0100

    Initial commit

---
+++ app/about/[slug]/page.jsx
@@ -0,0 +1,70 @@
+import matter from 'gray-matter'
+import { promises as fs } from 'fs'
+import { join } from 'path'
+import { serialize } from 'next-mdx-remote/serialize'
+
+import rehypeExternalLinks from 'rehype-external-links'
+import remarkSlug from 'remark-slug'
+
+import Container from '@component/Container'
+import MdxRemoteRender from '@component/MdxRemoteRender'
+
+const pagesPath = join(process.cwd(), '/src/data/pages')
+
+export async function generateMetadata({ params }) {
+  const { pageData } = await getPage(params)
+
+  return {
+    title: `${pageData.title} | HyperUI`,
+    description: pageData.description,
+    openGraph: {
+      title: `${pageData.title} | HyperUI`,
+      description: pageData.description,
+      url: 'https://www.hyperui.dev/',
+      siteName: 'HyperUI',
+      type: 'website',
+      image: 'https://www.hyperui.dev/og.jpg',
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: `${pageData.title} | HyperUI`,
+      description: pageData.description,
+    },
+  }
+}
+
+export async function generateStaticParams() {
+  return await fs.readdir(pagesPath)
+}
+
+async function getPage(params) {
+  const postPath = join(pagesPath, `${params.slug}.mdx`)
+  const postItem = await fs.readFile(postPath, 'utf-8')
+
+  const { content, data: frontmatter } = matter(postItem)
+
+  const mdxSource = await serialize(content, {
+    mdxOptions: {
+      remarkPlugins: [remarkSlug],
+      rehypePlugins: [[rehypeExternalLinks, { target: '_blank' }]],
+    },
+    scope: frontmatter,
+  })
+
+  return {
+    pageData: frontmatter,
+    pageContent: mdxSource,
+  }
+}
+
+export default async function Page({ params }) {
+  const { pageContent } = await getPage(params)
+
+  return (
+    <Container classNames="py-8 lg:py-12">
+      <article class="prose">
+        <MdxRemoteRender mdxSource={pageContent} />
+      </article>
+    </Container>
+  )
+}
