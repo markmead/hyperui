c6ee97edce1b38c9bc59228cc67aad40ae7b5536
Mark Mead <itsmarkmead@gmail.com>
2023-08-25 14:35:05 +0100

    Explanation comment

--- app/layout.jsx
+++ app/layout.jsx
@@ -37,22 +37,27 @@ const inter = Inter({
 export default function RootLayout({ children }) {
   return (
     <html className="h-full scroll-smooth" lang="en" dir="ltr">
+      {/*
+        Please note, GA is not a permanent solution,
+        I will be replacing it with Vercel in the future.
+      */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_KEY}`}
+      />
+
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.GA_KEY}');
+        `}
+      </Script>
+
       <head>
         <link rel="icon" href="/favicon.ico" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_KEY}`}
-        />
-        <Script id="google-analytics">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', '${process.env.GA_KEY}');
-        `}
-        </Script>
       </head>
 
       <body className={`${inter.variable} font-sans antialiased`}>
