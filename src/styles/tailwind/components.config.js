e387ded818664fd3aae3d7c0837e358784dea72d
Mark Mead <50486078+markmead@users.noreply.github.com>
2023-08-27 00:17:35 +0100

    Update - Marketing testimonials  (#364)

--- src/styles/tailwind/components.config.js
+++ src/styles/tailwind/components.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   darkMode: 'class',
   content: ['./public/components/**/*.html', './src/data/components/*.mdx'],
+  safelist: ['keen-slider', 'keen-slider__slide'],
   plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
   presets: [require('./extend.preset.js')],
 }
