0dc281b798e07c76ecef70b1acb4ffc68342e635
Mark Mead <itsmarkmead@gmail.com>
2023-09-29 10:33:42 +0100

    Initial commit

--- src/components/Ad.jsx
+++ src/components/Ad.jsx
@@ -1,11 +1,40 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
+
+import { useEffect, useState } from 'react'
+
 export default function Ad({ adType, adClass, adId }) {
+  const routerPathname = usePathname()
+
+  const [adPath, setAdPath] = useState('')
+
+  useEffect(() => {
+    if (routerPathname === '/') {
+      setAdPath('home')
+
+      return
+    }
+
+    const formattedPathname = routerPathname
+      .replace(/\//g, '-')
+      .replace(/^-/, '')
+      .replace(/components-/g, '')
+      .replace(/blog-/g, '')
+      .replace(/about-/g, '')
+
+    const newAdPath = `${adId}-${formattedPathname}`
+
+    setAdPath(newAdPath)
+  }, [routerPathname])
+
   return (
     <div className="not-prose mx-auto max-w-lg text-center">
       <div
         data-ea-publisher="hyperuidev"
         data-ea-type={adType}
         className={`${adClass} [&_.ea-callout]:!mb-0 [&_.ea-content]:!mx-0 [&_.ea-content]:!mt-0`}
-        id={adId}
+        id={adPath}
       ></div>
     </div>
   )
