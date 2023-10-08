70ed62eb3522d2e822760470808be89e2ee7ac94
Arnav K <arnavkaushal09@gmail.com>
2023-10-08 09:45:52 +0530

    Update not-found.jsx

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -20,24 +20,8 @@ export async function generateMetadata() {
 }
 
 export default function Custom404() {
-  /**
-   * Lists of ux 404 error messages
-  */
-  const errorMessages = [
-    "Oops! It looks like you've entered an incorrect URL. Double-check your web address and try again. 🌐",
-    "This page doesn't exist. You might have mistyped the URL. Please verify and try once more. 🔍",
-    "Looks like you're lost in cyberspace. Please recheck the URL and continue your journey. 🚀",
-    "This URL seems to be a little off course. Please review it and try another. 🌄 #WrongWay",
-    "Like typing the wrong address, this URL won't connect. Review it and try the right one. ☎️",
-    "You've stumbled upon a URL that doesn't lead anywhere. Please ensure it's correct and continue your digital journey. 🚴",
-    "Sorry, we couldn't find the page you're looking for. Please validate the URL, and we'll assist you in your search. 🧳"
-  ];
   return (
-    <section className="h-screen px-8 flex justify-center items-center">
-      <div className="text-center">
-        <h1 className="text-5xl font-extrabold">404!</h1>
-        <p className="my-2 text-lg break-words"> {errorMessages[Math.floor(Math.random() * errorMessages.length)]} </p>
-      </div>
-    </section>
-  )
+    <div className="grid h-screen px-4 bg-white place-content-center">
+  <h1 className="tracking-widest text-gray-500 uppercase text-3xl">404 | URL Not Found!</h1>
+</div>
 }
