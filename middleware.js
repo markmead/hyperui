025df2599a0120a0aac2c270a369166e1959fe1e
Mark Mead <itsmarkmead@gmail.com>
2024-02-06 14:27:10 +0000

    Initial commit

---
+++ middleware.js
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { Ratelimit } from '@upstash/ratelimit'
+import { kv } from '@vercel/kv'
+
+const ratelimit = new Ratelimit({
+  redis: kv,
+  // We limit it to 5 requests from the same IP within 10 seconds
+  limiter: Ratelimit.slidingWindow(5, '10 s'),
+})
+
+// Define which routes you want to rate limit
+export const config = {
+  matcher: '/',
+}
+
+export default async function middleware(nextRequest) {
+  const requestIp = nextRequest.ip ?? '127.0.0.1'
+
+  const { success: isSuccess } = await ratelimit.limit(requestIp)
+
+  return isSuccess
+    ? NextResponse.next()
+    : NextResponse.redirect(new URL('/blocked', nextRequest.url))
+}
