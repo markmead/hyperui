2959f1ec509218c389f87f2eafd438bafa09ff6d
Mark Mead <itsmarkmead@gmail.com>
2024-02-06 12:50:43 +0000

    Adjustable preview height

--- src/components/PreviewIframe.jsx
+++ src/components/PreviewIframe.jsx
@@ -3,6 +3,7 @@ export default function PreviewIframe({
   componentHtml,
   componentTitle,
   previewWidth = '100%',
+  previewHeight = 'h-[400px] lg:h-[600px]',
   refIframe,
   previewDark,
 }) {
@@ -16,7 +17,7 @@ export default function PreviewIframe({
       className="rounded-md bg-white bg-[linear-gradient(45deg,_rgb(249_250_251)_25%,_transparent_25%),_linear-gradient(-45deg,_rgb(249_250_251)_25%,_transparent_25%),_linear-gradient(45deg,_transparent_75%,_rgb(249_250_251)_75%),_linear-gradient(-45deg,_transparent_75%,_rgb(249_250_251)_75%)] bg-[length:_20px_20px] [background-position:_0_0,_0_10px,_10px_-10px,_-10px_0px] "
     >
       <iframe
-        className={`h-[400px] w-full rounded-md ring-2 ring-gray-900 lg:h-[600px] lg:transition-all ${iframeTheme}`}
+        className={`w-full rounded-md ring-2 ring-gray-900 lg:transition-all ${iframeTheme} ${previewHeight}`}
         loading="lazy"
         srcDoc={componentHtml}
         style={{ maxWidth: previewWidth }}
