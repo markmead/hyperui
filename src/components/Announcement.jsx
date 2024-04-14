8953c8cf9cd6c7456aac19d0725ce19ef48678f1
Mark Mead <itsmarkmead@gmail.com>
2024-04-14 16:06:48 +0100

    Hotfix - Clean up announcement

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -1,7 +1,7 @@
 export default function Announcement() {
   return (
-    <div className="fixed bottom-4 left-4 z-50 max-w-md" role="alert">
-      <div className="rounded-md border-2 border-gray-900 bg-white p-4 shadow-lg">
+    <div className="fixed bottom-4 z-50 p-4" role="alert">
+      <div className="max-w-sm rounded-md border-2 border-gray-900 bg-white p-4 shadow-lg">
         <p className="text-lg font-medium text-gray-900">
           Potential changes coming to HyperUI, have your say in the{' '}
           <a
