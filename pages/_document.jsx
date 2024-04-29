0f50819d8c135b1ef551f7c0cddb02bfd5751747
Mark Mead <itsmarkmead@gmail.com>
2024-04-29 14:49:01 +0100

    Fix document

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,7 +8,7 @@ export default function Document() {
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={true} />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap"
           rel="stylesheet"
