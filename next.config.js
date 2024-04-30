7e10ad225e3eadb18ee67964843f5e5e6aa3264e
Mark Mead <itsmarkmead@gmail.com>
2024-04-30 13:57:31 +0100

    Add errors

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+  trailingSlash: false,
 }
+
+module.exports = nextConfig
---
+++ pages/_404.jsx
@@ -0,0 +1,7 @@
+export default function Error404() {
+  return (
+    <div className="grid h-[600px] place-content-center bg-white px-4">
+      <h1 className="uppercase tracking-widest text-gray-700">404 | Not Found</h1>
+    </div>
+  )
+}
