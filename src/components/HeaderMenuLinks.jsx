e8807304359977965286c721f4d35afc6cd924de
Mark Mead <itsmarkmead@gmail.com>
2024-05-12 13:28:23 +0100

    Another clean up

--- src/components/HeaderMenuLinks.jsx
+++ src/components/HeaderMenuLinks.jsx
@@ -4,17 +4,11 @@ export default function HeaderMenuLinks({ menuLinks, navClass, ulClass }) {
   return (
     <nav aria-label="Global" className={navClass && navClass}>
       <ul className={ulClass && ulClass}>
-        {menuLinks.map((menuLink, linkIndex, { length }) => {
-          const isLast = linkIndex === length - 1
-
+        {menuLinks.map((menuLink) => {
           return (
-            <li key={menuLink.href} className={isLast ? 'lg:ms-auto' : ''}>
+            <li key={menuLink.href}>
               <Link
                 href={menuLink.href}
-                {...(menuLink.external && {
-                  target: '_blank',
-                  rel: 'noreferrer',
-                })}
                 className="text-sm font-medium text-gray-900 hover:opacity-75"
               >
                 {menuLink.title}
