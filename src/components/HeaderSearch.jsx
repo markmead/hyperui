d014967806eeec5188e820873158d48f5f666e7a
Mark Mead <itsmarkmead@gmail.com>
2024-05-26 13:07:47 +0100

    Remove sone lint warnings

--- src/components/HeaderSearch.jsx
+++ src/components/HeaderSearch.jsx
@@ -35,6 +35,7 @@ export default function HeaderSearch() {
     getSearchResults()
 
     return () => {}
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [showDropdown])
 
   useEffect(() => {
