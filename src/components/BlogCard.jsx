f2d2e5df1ce64af122285c8a19114a0c652011fb
Mark Mead <itsmarkmead@gmail.com>
2024-07-10 11:32:48 +0100

    Hotfix - Remove old link code

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function BlogCard({ blogPost }) {
   return (
-    <Link href="/blog/[slug]" as={`/blog/${blogPost.slug}`}>
+    <Link href={`/blog/${blogPost.slug}`}>
       <div className="group relative block h-full bg-white before:absolute before:inset-0 before:rounded-md before:border-2 before:border-dashed before:border-gray-900">
         <div className="h-full rounded-md border-2 border-gray-900 bg-white transition group-hover:-translate-y-2 ltr:group-hover:-translate-x-2 rtl:group-hover:translate-x-2">
           <div className="p-4 sm:p-6 lg:p-8">
