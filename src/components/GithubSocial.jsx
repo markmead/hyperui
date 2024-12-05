67fa4fee93d0415b7189325c059492c3cbf7a3f0
Mark Mead <itsmarkmead@gmail.com>
2024-12-05 13:54:26 +0000

    Hotfix - Bump star count

--- src/components/GithubSocial.jsx
+++ src/components/GithubSocial.jsx
@@ -10,7 +10,7 @@ export default function GithubSocial() {
 
       <IconGithub />
 
-      <span className="text-sm font-medium text-gray-700">10.1k</span>
+      <span className="text-sm font-medium text-gray-700">10.2k</span>
     </a>
   )
 }
