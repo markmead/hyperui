97cd60c073af47ac3f9f37352b4042314374f776
Mark Mead <itsmarkmead@gmail.com>
2025-03-03 21:36:58 +0000

    Trash project

--- src/components/MdxRemoteRender.jsx
+++
@@ -1,7 +0,0 @@
-'use client'
-
-import { MDXRemote } from 'next-mdx-remote'
-
-export default function MdxContent({ mdxSource, mdxComponents = {}, mdxScope = {} }) {
-  return <MDXRemote {...mdxSource} components={mdxComponents} scope={mdxScope} />
-}
