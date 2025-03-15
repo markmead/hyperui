1ceba47859ed9b37965c557354ecdf74a715daec
Mark Mead <itsmarkmead@gmail.com>
2025-03-15 20:07:50 +0000

    Hotfix - Remove text under USPs

--- src/components/UspList.jsx
+++ src/components/UspList.jsx
@@ -2,22 +2,16 @@ export default function UspList() {
   const uspItems = ['No install', 'No config', 'No setup']
 
   return (
-    <div>
-      <ul className="flex items-center justify-center gap-6">
-        {uspItems.map((uspItem, itemIndex) => (
-          <li className="inline-flex items-center gap-1" key={itemIndex}>
-            <span role="img" aria-hidden="true">
-              ✅
-            </span>
+    <ul className="flex items-center justify-center gap-6">
+      {uspItems.map((uspItem, itemIndex) => (
+        <li className="inline-flex items-center gap-1" key={itemIndex}>
+          <span role="img" aria-hidden="true">
+            ✅
+          </span>
 
-            <span className="text-sm font-medium text-gray-900">{uspItem}</span>
-          </li>
-        ))}
-      </ul>
-
-      <p className="mt-2 text-base/relaxed text-pretty text-gray-700">
-        Copy and paste components to build your next project.
-      </p>
-    </div>
+          <span className="text-sm font-medium text-gray-900">{uspItem}</span>
+        </li>
+      ))}
+    </ul>
   )
 }
