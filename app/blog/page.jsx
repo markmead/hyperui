0f244e9b96ba733573deafd8074eb308a79fd528
Mark Mead <itsmarkmead@gmail.com>
2025-04-12 12:32:53 +0100

    Escape `'`

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -19,8 +19,8 @@ export default async function Page() {
     <>
       <HeroBanner title="Tailwind CSS Blog" subtitle="Tips, Tricks & Real-World Solutions">
         Dive into this collection of Tailwind CSS insights that make development less painful and
-        more fun. Whether you're just starting out or have battle scars from countless projects,
-        there's something here to make your CSS life easier.
+        more fun. Whether you&#39;re just starting out or have battle scars from countless projects,
+        there&#39;s something here to make your CSS life easier.
       </HeroBanner>
 
       <Container id="mainContent" classNames="pb-8 lg:pb-12">
