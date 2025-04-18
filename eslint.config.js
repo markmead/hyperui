baceddad82bcfe9db62757ee8410f3b7c8035fa1
Mark Mead <itsmarkmead@gmail.com>
2025-04-18 10:25:38 +0100

    Clean up

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,8 @@ import { FlatCompat } from '@eslint/eslintrc'
 import js from '@eslint/js'
 import pluginReact from 'eslint-plugin-react'
 
-import * as pluginMdx from 'eslint-plugin-mdx'
-
 const compat = new FlatCompat({
-  //   baseDirectory: import.meta.dirname,
-  baseDirectory: import.meta.url,
+  baseDirectory: import.meta.dirname,
 })
 
 export default [
@@ -15,10 +12,6 @@ export default [
 
   js.configs.recommended,
   pluginReact.configs.flat.recommended,
-
-  {
-    ...pluginMdx.flat,
-  },
 
   ...compat.config({
     extends: ['next'],
