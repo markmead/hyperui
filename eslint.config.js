11d37b5556099d37e0870591e2c963af88f7cfdf
Mark Mead <itsmarkmead@gmail.com>
2025-04-18 10:14:06 +0100

    Setup plugins

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,26 @@
-// ESLint flat config using FlatCompat for legacy config support (JS only)
 import { FlatCompat } from '@eslint/eslintrc'
 
+import js from '@eslint/js'
+import pluginReact from 'eslint-plugin-react'
+
+import * as pluginMdx from 'eslint-plugin-mdx'
+
 const compat = new FlatCompat({
+  //   baseDirectory: import.meta.dirname,
   baseDirectory: import.meta.url,
 })
 
 export default [
-  ...compat.extends('next'),
+  { ignores: ['.next', 'node_modules'] },
+
+  js.configs.recommended,
+  pluginReact.configs.flat.recommended,
+
   {
-    ignores: ['**/*.ts', '**/*.tsx'],
-    rules: {
-      '@next/next/no-img-element': 'off',
-      curly: 'warn',
-      'default-case': 'warn',
-      'default-param-last': 'warn',
-      'dot-notation': 'warn',
-      eqeqeq: 'error',
-      'no-alert': 'error',
-      'no-console': 'warn',
-      'no-debugger': 'warn',
-      'no-duplicate-imports': 'error',
-      'no-else-return': 'warn',
-      'no-empty': 'warn',
-      'no-unused-vars': 'warn',
-      'prefer-const': 'warn',
-      'prefer-promise-reject-errors': 'off',
-      'vue/multi-word-component-names': 'off',
-      quotes: ['warn', 'single', { allowTemplateLiterals: false, avoidEscape: true }],
-      'sort-imports': [
-        'warn',
-        {
-          ignoreDeclarationSort: true,
-        },
-      ],
-    },
+    ...pluginMdx.flat,
   },
+
+  ...compat.config({
+    extends: ['next'],
+  }),
 ]
