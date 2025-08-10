e2cf35679e25955393994613ba6aeae57cc15a9f
Mark Mead <itsmarkmead@gmail.com>
2025-08-10 09:32:04 +0100

    Update ESLint config for React and output directory
    Renamed React plugin import to 'react' for consistency and added 'out' to ignored directories in ESLint configuration.

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,17 @@
 import { FlatCompat } from '@eslint/eslintrc'
 
 import js from '@eslint/js'
-import pluginReact from 'eslint-plugin-react'
+import react from 'eslint-plugin-react'
 
 const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
 })
 
 export default [
-  { ignores: ['.next', 'node_modules'] },
+  { ignores: ['.next', 'node_modules', 'out'] },
 
   js.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  react.configs.flat.recommended,
 
   ...compat.config({
     extends: ['next'],
