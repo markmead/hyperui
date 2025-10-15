eb6d7aadbe820ff0e053cb637ef6608583300a3b
Mark Mead <itsmarkmead@gmail.com>
2025-10-15 14:54:43 +0100

    Add Ad component to MDX pages and update Ad usage
    Introduces the Ad component into various MDX documentation pages, placing <Ad adType="text" /> after the description in each. Refactors the Ad component to accept an adType prop and updates its styling. Removes the Ad component from the global layout and instead adds it to the Hero section and makes it available in MDX rendering via MdxRemoteRender.

--- src/components/MdxRemoteRender.jsx
+++ src/components/MdxRemoteRender.jsx
@@ -2,11 +2,12 @@
 
 import { MDXRemote } from 'next-mdx-remote'
 
+import Ad from '@component/global/Ad'
 import BlogCallout from '@component/BlogCallout'
 import CollectionList from '@component/CollectionList'
 
 export default function MdxContent({ mdxSource, mdxScope = {} }) {
-  const mdxComponents = { BlogCallout, CollectionList }
+  const mdxComponents = { Ad, BlogCallout, CollectionList }
 
   return <MDXRemote {...mdxSource} scope={mdxScope} components={mdxComponents} />
 }
