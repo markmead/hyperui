b569eac5c6685c02daf7c0aa7b54d67183a16c1d
Mark M <50486078+markmead@users.noreply.github.com>
2025-11-14 14:49:19 +0000

    Feature - Add new progress bar and skip link components (#662)

--- public/component.js
+++ public/component.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('a, input[type="file"]').forEach((iframeLink) => {
     iframeLink.addEventListener('click', (e) => e.preventDefault())
-    iframeLink.addEventListener('keydown', (e) => e.preventDefault())
+    iframeLink.addEventListener('keydown', (e) => e.key !== 'Tab' && e.preventDefault())
   })
 
   document.querySelectorAll('form').forEach((iframeForm) => {
